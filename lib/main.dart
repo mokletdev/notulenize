05ac660b8fbae7c4c18943b543fffc13da5d59e0
Devdit <pradita.cleine@gmail.com>
2024-12-12 10:21:59 +0700

    feat: add api key to openai model

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
+import 'package:dart_openai/dart_openai.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:notulenize/audio_to_text.dart';
+import 'package:notulenize/splash_screen.dart';
+Future<void> main() async {
+  // Ensure that widget binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  runApp(const MainApp());
+  // Load the .env file
+  await dotenv.load(fileName: ".env");
+
+  OpenAI.apiKey = dotenv.env['API_KEY']!;
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+      routes: {
+        // Define your named routes here
+        '/audio_to_text': (context) =>
+            AudioToTextPage(), // Add the route for AudioToTextPage
+        // You can add more routes as needed, for example:
+        // '/summary_page': (context) => SummaryPage(),
+        // '/another_page': (context) => AnotherPage(),
+      },
     );
   }
 }
