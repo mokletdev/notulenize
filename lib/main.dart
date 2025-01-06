60d4f5c4919f74edf898b79198477b462f1ffbc8
Devdit <pradita.cleine@gmail.com>
2025-01-06 07:06:34 +0700

    chore: remove unecessary comment

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:notulenize/audio_to_text.dart';
 import 'package:notulenize/splash_screen.dart';
 Future<void> main() async {
-  // Ensure that widget binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Load the .env file
   await dotenv.load(fileName: ".env");
 
   OpenAI.apiKey = dotenv.env['API_KEY']!;
@@ -24,12 +22,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
       routes: {
-        // Define your named routes here
         '/audio_to_text': (context) =>
-            AudioToTextPage(), // Add the route for AudioToTextPage
-        // You can add more routes as needed, for example:
-        // '/summary_page': (context) => SummaryPage(),
-        // '/another_page': (context) => AnotherPage(),
+            AudioToTextPage()
       },
     );
   }
